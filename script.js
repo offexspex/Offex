3b04cc2fb502ea4ffee26cb14468623d01c7b88e
anisirji <www.allinoneforyouhere@gmail.com>
2021-10-26 06:32:23 +0530

    minor updates and course page added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const sec = document.getElementById("secs");
 // const date = window.prompt(
 //   "Enter Date in dd/mm/yyyy format and time in hh:mm:ss format followed by space"
 // );
-date = "15 oct 2021";
+date = "1 dec 2021";
 const myDate = new Date(date); //using New DATE OBJECT
 
 function timer() {
